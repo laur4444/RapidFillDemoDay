0462f9b3bee925c6047a0944a132100d7b06784f
andreiseri <andreiseri@users.noreply.github.com>
2018-05-20 02:07:39 +0300

    Adaugat harta cu markere

--- app/src/main/java/net/ddns/rapidfill/rapidfilldemoday/Home.java
+++ app/src/main/java/net/ddns/rapidfill/rapidfilldemoday/Home.java
@@ -1,14 +1,23 @@
 package net.ddns.rapidfill.rapidfilldemoday;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.Voice;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -19,29 +28,47 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class Home extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener
-        {
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     Context context;
 
     private GoogleMap mMap;
+    AlertDialog commandDialog;
 
     SpeechActions speech;
     TextToSpeech toSpeech;
 
+    FloatingActionButton fab;
+
+    //Location
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private double user_lat, user_long;
+
+    private Circle circle;
+    private boolean added = false;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,7 +84,7 @@ public class Home extends AppCompatActivity
                 if (status == TextToSpeech.SUCCESS) {
                     int result = toSpeech.setLanguage(Locale.ENGLISH);
 
-                    if(result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
                         Log.e("TTS", "language not supported");
                         Toast.makeText(Home.this, "Nu merge comanda vocala", Toast.LENGTH_SHORT).show();
                     }
@@ -69,7 +96,8 @@ public class Home extends AppCompatActivity
 
         speech = new SpeechActions(toSpeech, context);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.hide();
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -92,11 +120,78 @@ public class Home extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        MapsFragment fragment = new MapsFragment();
-        android.support.v4.app.FragmentTransaction fragmentTransaction =
-                getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.map, fragment);
-        fragmentTransaction.commit();
+
+        // MAP si LOCATION
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                user_lat = location.getLatitude();
+                user_long = location.getLongitude();
+                if (added) {
+                    circle.remove();
+                } else {
+                    LatLng pos = new LatLng(user_lat, user_long);
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(pos, 16));
+                }
+                CircleOptions circleOptions = new CircleOptions()
+                        .center(new LatLng(user_lat, user_long))
+                        .radius(3);
+
+                circle = mMap.addCircle(circleOptions);
+                added = true;
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            requestPermissions(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET
+            }, 10);
+            return;
+        } else {
+            configureLocation();
+        }
+
+
+    }
+
+    public void configureLocation(){
+        locationManager.requestLocationUpdates("gps", 5000, 5, locationListener);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        switch (requestCode){
+            case 10:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    configureLocation();
+                }
+        }
     }
 
     @Override
@@ -204,4 +299,70 @@ public class Home extends AppCompatActivity
         }
         super.onDestroy();
     }
+
+
+    // Harta
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        double[] latitudine = {44.420, 44.431};
+        double[] longitudine = {26.088, 26.110};
+        String[] numeBenzinarie = {"Strada Gazelei", "Bulevardul Corneliu Coposu"};
+        for (int i = 0 ; i < 2 ; ++i){
+            LatLng coord = new LatLng(latitudine[i], longitudine[i]);
+            Marker marker = mMap.addMarker(new MarkerOptions().position(coord).title(numeBenzinarie[i]));
+            marker.setTag("" + i + "_" + numeBenzinarie[i]);
+        }
+        mMap.setOnMarkerClickListener(this);
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        String oras = (String) marker.getTag();
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marker.getPosition(), 16));
+        confirmCommand(oras);
+        return false;
+    }
+
+    void confirmCommand(String oras){
+        final AlertDialog.Builder commandDialogAux;
+        final View markerBenzinarie;
+        TextView benzinarieText;
+        Button confirmCommandButton;
+        Button cancelButton;
+
+        commandDialogAux = new AlertDialog.Builder(this);
+        markerBenzinarie = getLayoutInflater().inflate(R.layout.marker_benzinarie, null);
+        benzinarieText = (TextView) markerBenzinarie.findViewById(R.id.benzinarieTextView);
+        confirmCommandButton = (Button) markerBenzinarie.findViewById(R.id.btnComanda);
+        cancelButton = (Button) markerBenzinarie.findViewById(R.id.btnCancel);
+
+        final String tokenize[] = oras.split("_");
+
+        benzinarieText.setText(tokenize[1]);
+        confirmCommandButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "Comanda " + tokenize[0], Toast.LENGTH_SHORT).show();
+                commandDialog.dismiss();
+                fab.show();
+            }
+        });
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                commandDialog.dismiss();
+            }
+        });
+        commandDialogAux.setView(markerBenzinarie);
+        commandDialog = commandDialogAux.create();
+        commandDialog.setCancelable(true);
+        commandDialog.show();
+    }
+
+
+
+
 }
--- app/src/main/java/net/ddns/rapidfill/rapidfilldemoday/MapsActivity.java
+++ app/src/main/java/net/ddns/rapidfill/rapidfilldemoday/MapsActivity.java
@@ -1,18 +1,27 @@
 package net.ddns.rapidfill.rapidfilldemoday;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap;
+    private Marker mSydney;
+    AlertDialog commandDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +37,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng sydney = new LatLng(-34 , 151);
+        mSydney = mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mSydney.setTag("1_Sydney");
+        mMap.setOnMarkerClickListener(this);
+    }
+
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+        String oras = (String) marker.getTag();
+        confirmCommand(oras);
+        return false;
+    }
+
+    void confirmCommand(String oras){
+        final AlertDialog.Builder commandDialogAux;
+        final View markerBenzinarie;
+        TextView benzinarieText;
+        Button confirmCommandButton;
+        Button cancelButton;
+
+        commandDialogAux = new AlertDialog.Builder(MapsActivity.this);
+        markerBenzinarie = getLayoutInflater().inflate(R.layout.marker_benzinarie, null);
+        benzinarieText = (TextView) markerBenzinarie.findViewById(R.id.benzinarieTextView);
+        confirmCommandButton = (Button) markerBenzinarie.findViewById(R.id.btnComanda);
+        cancelButton = (Button) markerBenzinarie.findViewById(R.id.btnCancel);
+
+        final String tokenize[] = oras.split("_");
+
+        benzinarieText.setText(tokenize[1]);
+        confirmCommandButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MapsActivity.this, "Comanda " + tokenize[0], Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                commandDialog.dismiss();
+            }
+        });
+        commandDialogAux.setView(markerBenzinarie);
+        commandDialog = commandDialogAux.create();
+        commandDialog.setCancelable(true);
+        commandDialog.show();
     }
 }
